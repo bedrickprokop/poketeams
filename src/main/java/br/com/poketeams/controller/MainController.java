1a4122a7ee7bde212cc8dda7773409c46318b974
Bedrick Prokop <bedrick.prokop@zup.com.br>
2018-03-13 17:00:20 -0300

    teste

--- src/main/java/br/com/poketeams/controller/MainController.java
+++ src/main/java/br/com/poketeams/controller/MainController.java
@@ -9,14 +9,6 @@ public class MainController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String homepage() {
-<<<<<<< HEAD
-        return "index" //poketeams 1 - lll;
-=======
-<<<<<<< HEAD
-        return "index" //poketeams2;
-=======
-        return "index" //poketeams 1;
->>>>>>> e7dc724a29208551a58ad7c122dd07ed27ec553e
->>>>>>> 8f2f353c1f81713dae9043f392e7b4d9ee7d78d4
+        return "index" //poketeams 1 final;
     }
 }
